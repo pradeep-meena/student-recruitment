d00412a8169a07737fcc5d9a50f0a75e896a0b0e
pradeep meena <152312832+pradeep-meena@users.noreply.github.com>
2025-02-07 21:44:14 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { useState } from "react";
 
 
 import Login from "./authtication/Login";
+import Dashbord from "./components/dashbord/Dashbord";
 
 function App() {
   const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(false);
@@ -44,6 +45,7 @@ function App() {
             {/* login signup */}
 
             {/* dashbord */}
+            <Route path="/dashboard" element={<Dashbord />} />
            
             {/* dashbord */}
 
